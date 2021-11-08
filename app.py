13856db958f6635c8adedf3bbb1ad71ba3f5bd99
Jimmy <jarm297@gmail.com>
2021-11-07 20:14:41 -0500

    Update predict function

--- app.py
+++ app.py
@@ -51,26 +51,26 @@ def test(config_file: str):
         yaml.dump(content, f)
 
 @app.command()
-def predict(config_file: str, shop_id: int, category_id: int):
+def predict(config_file: str, shop_id: int, category_id: int, predict_last: bool):
     data_config = _load_config(config_file, "data")['filepath']
     model_path = _load_config(config_file, "model")['filepath']
-    # Find the corresponding shop/category info:
 
+    # Find the corresponding shop/category info:
     df = pd.read_csv(data_config)
     row = df[df['id']==str((shop_id, category_id))]
     relevant_columns = ['shop_id', 'item_category_id']
-    columns_to_drop = list(
-        [column for column in row.columns if (column != 'shop_id' and column != 'item_category_id')])
-
-    for i in range(3):
-        columns_to_drop.pop()
     
+    # Just for comparing purpose
+    for i in (range(-3,0) if predict_last else range(-4,-1)):
+        relevant_columns.append(row.columns[i])
+    columns_to_drop = list(
+        [column for column in row.columns if column not in relevant_columns])
     row = row.drop(columns=columns_to_drop)
 
     # Use the model:
     model = joblib.load(model_path)
     prediction = model.predict(row)
-    print(prediction)
+    print(prediction[0])
     return prediction[0]
 
 def _get_dataset(data_config):
